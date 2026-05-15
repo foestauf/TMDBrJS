2b775ea611e1f6fc6c32a42b00e0bc4aa204ca4e
Robert McKee <foestauf@gmail.com>
2026-05-15 12:31:16 -0400

    feat(movies): add getLists

--- src/movies/types/Lists.ts
+++ src/movies/types/Lists.ts
@@ -7,3 +7,18 @@ export interface DatedMovieList extends Paginated<Movie> {
 
 export type NowPlayingMovies = DatedMovieList;
 export type UpcomingMovies = DatedMovieList;
+
+export interface MovieListItem {
+  description: string;
+  favoriteCount: number;
+  id: number;
+  itemCount: number;
+  iso6391: string;
+  listType: string;
+  name: string;
+  posterPath: string | null;
+}
+
+export interface MovieLists extends Paginated<MovieListItem> {
+  id: number;
+}
